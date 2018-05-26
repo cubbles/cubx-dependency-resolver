1a9bc41cae7117b45cbc95edc66b8e7a1a647eed
edwingamboa <edwingamboa355@gmail.com>
2018-05-26 15:35:00 -0500

    cubbles/cubbles#12: Add bin script

---
+++ bin/cubx-dependency-resolver.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var ArtifactsDepsResolver = require('../lib/cubx-dependency-resolver.js');
+var commandLineArgs = require('command-line-args');
+
+var args = [
+  { name: 'baseUrl', type: String, alias: 'u' },
+  { name: 'rootDependencies', type: String, alias: 'd' }
+];
+
+var options = commandLineArgs(args);
+
+if (!options.baseUrl) {
+  console.error('Missed necessary parameter "webpackagePath". Usage: cubx-webpackage-rte-update -p <webpackagPath>');
+  process.exit(0);
+}
+
+if (!options.rootDependencies) {
+  console.error('Missed necessary parameter "newRteVersion". Usage: cubx-webpackage-rte-update -v <newRteVersion>');
+  process.exit(0);
+} else {
+  try {
+    options.rootDependencies = JSON.parse(options.rootDependencies);
+  } catch (e) {
+    console.error('\'rootDependencies\' property should be a valid JSON object.', e);
+  }
+}
+
+var rteUpdater = new ArtifactsDepsResolver();
+rteUpdater.buildRawDependencyTree(options.rootDependencies, options.baseUrl);
