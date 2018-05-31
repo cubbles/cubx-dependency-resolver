1e195ca04f21a74b60c911e348a1740d8c96eac7
edwingamboa <edwingamboa355@gmail.com>
2018-05-31 10:36:36 -0500

    cubbles/cubbles#12: Now it is possible to provide the rootDeps as a file, using its path

--- bin/cubx-dependency-resolver.js
+++ bin/cubx-dependency-resolver.js
@@ -1,28 +1,44 @@
-'use strict';
-
+#! /usr/bin/env node
 var ArtifactsDepsResolver = require('../lib/cubx-dependency-resolver.js');
 var commandLineArgs = require('command-line-args');
+var fs = require('fs-extra');
 
 var args = [
-  { name: 'baseUrl', type: String, alias: 'u' },
-  { name: 'rootDependencies', type: String, alias: 'd' }
+  {name: 'baseUrl', type: String, alias: 'u'},
+  {name: 'rootDependencies', type: String, alias: 'd'}
 ];
+
+function parseRootDependencies (rootDeps) {
+  try {
+    rootDeps = JSON.parse(rootDeps);
+    if (!Array.isArray(rootDeps)) {
+      throw new TypeError('\'rootDependencies\' is not a valid JSON list');
+    }
+    return rootDeps;
+  } catch (e) {
+    console.error('\'rootDependencies\' property should be a valid JSON array or a path to a file containing a valid JSON array.', e);
+    process.exit(0);
+  }
+}
 
 var options = commandLineArgs(args);
 
 if (!options.baseUrl) {
-  console.error('Missed necessary parameter "webpackagePath". Usage: cubx-webpackage-rte-update -p <webpackagPath>');
+  console.error('Missed necessary parameter "baseUrl". Usage: cubx-dependency-resolver -u <baseUrl>');
   process.exit(0);
 }
 
 if (!options.rootDependencies) {
-  console.error('Missed necessary parameter "newRteVersion". Usage: cubx-webpackage-rte-update -v <newRteVersion>');
+  console.error('Missed necessary parameter "rootDependencies". Usage: cubx-dependency-resolver -d <rootDependencies>');
   process.exit(0);
 } else {
   try {
-    options.rootDependencies = JSON.parse(options.rootDependencies);
+    if (fs.pathExists(options.rootDependencies)) {
+      options.rootDependencies = fs.readFileSync(options.rootDependencies, 'utf8');
+      options.rootDependencies = parseRootDependencies(options.rootDependencies);
+    }
   } catch (e) {
-    console.error('\'rootDependencies\' property should be a valid JSON array.', e);
+    options.rootDependencies = parseRootDependencies(options.rootDependencies);
   }
 }
 
